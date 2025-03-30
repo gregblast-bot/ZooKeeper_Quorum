1cf99e55ed84d1f6f850e29e6285e3f6b1fadfd4
Gregory Wagonblast <ghw3@pitt.edu>
2025-03-30 00:05:49 -0400

    separate tests

--- ZooKeeper_Quorum/client.py
+++ ZooKeeper_Quorum/client.py
@@ -70,30 +70,29 @@ def main():
         # All updates routed through server on the elected leader. Sending through port 5000 as default.
         add_update(host, ports[0], f"key0", f"value0")
 
-        time.sleep(10) # Wait for server to update
+        #time.sleep(10) # Wait for server to update
 
         for i in range(len(ports)):
             for j in range(len(ports)):
                 print(f"\033[36mFor Port: {ports[i]}\033[0m")
                 read_key(host, ports[i], f"key{j}") # Check existing keys on all ports
 
-        time.sleep(10) # Wait for server to update
+        #time.sleep(10) # Wait for server to update
 
         print("\033[32mTesting Add and Read Through Replicas...\033[0m")
         # All updates routed through server on the elected leader. Sending through port 5002.
         add_update(host, ports[2], f"key2", f"value2")
 
-        time.sleep(10) # Wait for server to update
+        #time.sleep(10) # Wait for server to update
 
         for i in range(len(ports)):
             for j in range(len(ports)):
                 print(f"\033[36mFor Port: {ports[i]}\033[0m")
                 read_key(host, ports[i], f"key{j}") # Check existing keys on all ports
 
-        time.sleep(10) # Wait for server to update
+        #time.sleep(10) # Wait for server to update
 
         killReplica = False
-        i = 0
         for port, server in servers:
             response = kill(host, port)
             is_leader = response.get("is_leader", False)
@@ -104,9 +103,13 @@ def main():
                 killReplica = True
                 start_server(host, port, zookeeper_ip, zookeeper_port)
                 print("\033[33mReading from replica.\033[0m")
-                read_key(host, port, f"key{i}") # Read from killed replica
-                time.sleep(10) # Wait for server to start
-                read_key(host, port, f"key{i}") # Read from killed replica
+                read_key(host, port, f"key0") # Read from killed replica
+                read_key(host, port, f"key1") # Read from killed replica
+                read_key(host, port, f"key2") # Read from killed replica
+
+        for port, server in servers:
+            response = kill(host, port)
+            is_leader = response.get("is_leader", False)
             if is_leader:
                 print("\033[32mTesting Leader Election...\033[0m")
                 print("\033[31mKilling the leader, electing a new one.\033[0m")
@@ -115,7 +118,6 @@ def main():
                 start_server(host, port, zookeeper_ip, zookeeper_port)
                 print("\033[32mTesting Timeout Logic...\033[0m")
                 timeout(host, port) # Enable server timeout.
-            i += 1
 
         print("\033[32mTesting Stale Read...\033[0m")
         for i in range(len(ports)):
